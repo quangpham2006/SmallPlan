680ceea828759964a225e90b9113e10e9edcc244
khointn <nguyentietnguyenkhoi@gmail.com>
2025-08-27 08:47:04 +0000

    debug inference

--- src/slm_api/app.py
+++ src/slm_api/app.py
@@ -2,6 +2,7 @@ import sys
 import logging
 import yaml
 from fastapi import FastAPI
+from argparse import ArgumentParser
 
 from dotenv import load_dotenv
 sys.path.append('/workspace/khointn/SmallPlan')
@@ -12,12 +13,24 @@ logger= logging.getLogger(__name__)
 from src.slm_api.setup_trainer import SLMTrainer
 from src.slm_api.schema import Message, Conversation
 
+parser = ArgumentParser()
+parser.add_argument("--mode", type=str, choices=["train", "eval"], required=True, help="Mode: 'train' or 'eval'")
+mode = str(parser.parse_args().mode)
+
 with open('configs/slm_training.yaml', 'r') as file:
             cfg = yaml.safe_load(file)
 
+if mode == "train":
+    model_path = f"{cfg['preadapted_outputs_path']}/{cfg['slm_api_model']}"
+elif mode == "eval":
+    model_path = f"{cfg['smallplan_outputs_path']}/{cfg['strategy']}-2e-{cfg['slm_api_model']}/{cfg['last_train_scene']}"
+else:
+     raise ValueError("Mode must be either 'train' or 'eval'")
+
 print(f"Using model: {cfg['slm_api_model']}")
+print(f"Model path: {model_path}")
 app = FastAPI()
-smallplan_trainer = SLMTrainer(model_name=f"{cfg['preadapted_outputs_path']}/{cfg['slm_api_model']}")
+smallplan_trainer = SLMTrainer(model_name=model_path, mode=mode)
 
 '''
 The simulation and the training runs in 2 different environments (igibson vs smallplan).
